2f43c1e881287d1a37a6e84a970585da9c4465ff
Michele <hello@michelebosi.com>
2016-01-13 17:33:09 +0000

    added font awesome, blah.js, long.jade, register.jade

---
+++ public/javascripts/blah.js
@@ -0,0 +1,55 @@
+var Blah = function() 
+{
+    "use strict";
+    var socket = io.connect('http://localhost:3000');
+
+    // chat view route
+    if (document.location.pathname == '/') {
+        var seen_messages = new Set();
+        socket.on('append-messages', function (data) {
+            // counter = data.count;
+            var messages = $('#messages')[0];
+            data.new_messages.forEach(function(msg_data) {
+                if (!seen_messages.has(msg_data._id)) {
+                    seen_messages.add(msg_data._id)
+                    var p = document.createElement('p');
+                    var date = new Date(msg_data.time);
+                    var date_string = date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+                    var template = _.template("<blockquote><p>(<%= _id %>) <b><%= author %></b>: <%= message %></p><footer><%= date_string %></footer></blockquote>");
+                    $(p).html(template({
+                        _id: msg_data._id,
+                        author: msg_data.author,
+                        message: msg_data.message,
+                        date_string: date_string
+                    }));
+                    // $(p).html(msg_data._id + ': ' + msg_data.message + '<br><footer>' + date_string + '</footer>');
+                    messages.insertBefore(p, messages.firstChild);
+                }
+            });
+        });
+        socket.on('post-message-ok', function(data) {
+            $('#message')[0].value = '';
+            $('#message')[0].readOnly = false;
+        });
+    }
+
+    // self reveal Blah module
+
+    return {
+        sendMessage: function() {
+            $('#message')[0].readOnly = true;
+            var message = $('#message')[0].value;
+            socket.emit('post-message', {
+                message: message
+            });
+        },
+        
+        register: function() {
+            console.log('register');
+        },
+
+        login: function() {
+            console.log('login');
+        }
+    }
+}();
