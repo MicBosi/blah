ea8cf37ad87446a9e3e2fa70037b134507c309f9
Michele <hello@michelebosi.com>
2016-01-13 13:24:19 +0000

    added socket.io support

--- app.js
+++ app.js
@@ -5,10 +5,98 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+// ----------------------------------------------
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+// ----------------------------------------------
+
+var debug = require('debug')('blah2:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+// ----------------------------------------------
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +109,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var routes = require('./routes/index')(io);
+var users = require('./routes/users');
 
 app.use('/', routes);
 app.use('/users', users);
@@ -59,5 +150,7 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+// ----------------------------------------------
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,51 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(io) 
+{ 
+    var express = require('express');
+    var router = express.Router();
 
-var messages = ["Welcome!"];
-var clients = [];
+    var messages = ["Welcome!"];
+    var clients = [];
 
-/* home page */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Blah' });
-});
+    /* home page */
+    router.get('/', function(req, res, next) {
+        res.render('index', { title: 'Blah' });
+    });
 
-/* poll new messages */
-router.get('/poll/:count', function(req, res, next) {
-    var count = req.params.count;
-    if(messages.length > count) {
-        res.type('application/json');
-        res.send(JSON.stringify({
-            count: messages.length,
-            new_messages: messages.slice(count)
-        }));
-    } else {
-        clients.push(res);
-    }
-});
+    /* poll new messages */
+    router.get('/poll/:count', function(req, res, next) {
+        var count = req.params.count;
+        if(messages.length > count) {
+            res.type('application/json');
+            res.send(JSON.stringify({
+                count: messages.length,
+                new_messages: messages.slice(count)
+            }));
+        } else {
+            clients.push(res);
+        }
+    });
 
-/* send new message */
-router.post('/msg/', function(req, res, next) {
-    var message = req.body.message;
-    messages.push(message);
-    while(clients.length > 0) {
-        var client = clients.pop();
-        client.type('application/json');
-        client.end(JSON.stringify({
-            count: messages.length,
-            new_messages: [message]
-        }));
-    }
-    res.end();
-});
+    /* send new message */
+    router.post('/msg/', function(req, res, next) {
+        var message = req.body.message;
+        messages.push(message);
+        while(clients.length > 0) {
+            var client = clients.pop();
+            client.type('application/json');
+            client.end(JSON.stringify({
+                count: messages.length,
+                new_messages: [message]
+            }));
+        }
+        res.end();
+    });
 
-module.exports = router;
+    io.on('connection', function (socket) {
+        socket.emit('news', { hello: 'world' });
+        socket.on('my other event', function (data) {
+            console.log(data);
+        });
+    });
+
+    return router;
+}
