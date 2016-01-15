02520d79dcf71843b9ea8184a6635d15eb69d6cb
Michele <hello@michelebosi.com>
2016-01-15 13:41:46 +0000

    remote test

--- public/javascripts/blah.js
+++ public/javascripts/blah.js
@@ -1,7 +1,7 @@
 var Blah = function() 
 {
     "use strict";
-    var socket = io.connect('//:80');
+    var socket = io.connect('//:3000');
 
     // chat view route
     if (document.location.pathname == '/') {
